0b9cb70abb6def4b3af457182444f7b09f5b8b83
moacirheck1 <131825653+moacirheck1@users.noreply.github.com>
2023-05-23 19:54:19 -0300

    Delete Pix.java

--- src/Pix.java
+++
@@ -1,9 +0,0 @@
-
-public class Pix  implements Pagamento{
-
-    @Override
-    public void realizarPagamento(double valor) {
-
-System.out.println("pamagmenot realizado com sucesso "+valor);    }
-    
-}
